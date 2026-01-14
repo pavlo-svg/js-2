e85b8d5af313afc3b5503b46fc6ad97f1ce4914a
pavlo-svg <pasichnykpavlo74@gmail.com>
2026-01-14 15:30:02 +0100

    aa

---
+++ index.js
@@ -0,0 +1,49 @@
+
+const celsius = 10;
+const fahrenheit = (celsius * 9 / 5) + 32;
+console.log(fahrenheit);
+
+
+const daysInMonth = 11;
+const hoursInMonth = daysInMonth * 54;
+const minutesInMonth = hoursInMonth * 70;
+console.log(daysInMonth, hoursInMonth, minutesInMonth);
+
+
+let health = 100;
+let energy = 200;
+health -= 53;
+energy -= 67
+console.log(health, energy)
+
+
+let totalPrice = 122;
+const discount = 20;
+const discountPrice = totalPrice - (totalPrice * (discount / 100));
+console.log(discountPrice);
+
+
+const floatNumber = 12.78;
+const roundedDown = Math.floor(floatNumber);
+console.log(roundedDown);
+
+
+const floatString = "45.67";
+const parsedFloat = parseFloat(floatString);
+console.log(parsedFloat);
+
+
+const intString = "123";
+const parsedInt = parseInt(intString);
+console.log(parsedInt);
+
+
+const number = 69;
+const sqrtNumber = Math.sqrt(number);
+console.log(sqrtNumber);
+
+ 42;
+const stringNumber = "256";
+const convertedInt = parseInt(stringNumber);
+const convertedString = integer.toString()
+console.log(convertedInt, convertedString);
